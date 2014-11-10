e79540105348d9cc624f8711484d0fb49075b0f0
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2014-11-10 16:14:16 +0100

    arrays can't be exported, give them as params

--- publish_status.sh
+++ publish_status.sh
@@ -3,14 +3,13 @@
 # This script allows to publish the jenkins build status
 #
 # Its behaviour is modified by the following environment variables:
-#   BUILDS: the directory in which the documentation has been generated
 #   TARGET_URL: the url to copy to
 
 #BUILDS="rock-basic rock"
 
 echo "<html lang=\"en\"><head><meta charset=\"utf-8\"><title>Rock Current Build Status</title></head><body>" > status/status.html
 
-for build in ${BUILDS}; do
+for build in $@; do
 echo "getting status of $build"
 ruby ./extract_status.rb > "status/$build.html"
 echo "<h1>$build</h1>" >> status/status.html
