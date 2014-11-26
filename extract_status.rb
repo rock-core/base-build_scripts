b41ec41c2d51df57eef3d3964d94a97f012c399b
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2014-11-26 14:39:04 +0100

    output table after changes

--- extract_status.rb
+++ extract_status.rb
@@ -45,7 +45,6 @@ end
 #end
 table.xpath('//@class').remove
 
-puts table.to_html
 
 #copy log files
 table.css('a').each do |a|
@@ -69,4 +68,4 @@ table.css('a').each do |a|
     end
 end
 
-
+puts table.to_html
