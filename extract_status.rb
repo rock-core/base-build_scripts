db3f59c9a70935584b0f31c104a9922756597a8e
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2014-11-26 11:39:34 +0100

    also copy build logs

--- extract_status.rb
+++ extract_status.rb
@@ -2,14 +2,16 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'fileutils'
 
-page = Nokogiri::HTML(open("http://buildsrv01:8080/job/#{ARGV[0]}/"))
+base_url="http://buildsrv01:8080"
+
+page = Nokogiri::HTML(open("#{base_url}/job/#{ARGV[0]}/lastBuild"))
 
 page.remove_namespaces!
 
 
 table = page.at_css('#configuration-matrix')
-
 
 table.xpath('//img').each do |img|
     if img["src"].include? "red.png"
@@ -43,4 +45,22 @@ end
 #end
 table.xpath('//@class').remove
 
+#copy log files
+table.css('a').each do |a|
+    if a["href"]
+        consoleurl = base_url + a["href"] + "consoleText"
+        consolefileurl = a["href"] + "console"
+        console = open(consoleurl)
+	FileUtils.mkdir_p File.dirname("status"+consolefileurl)
+	consolecopy = File.open("status"+consolefileurl,'w')
+	console.each_line do |line|
+	    consolecopy.write(line)
+	end
+	consolecopy.close
+	console.close
+	a["href"] = consolefileurl[1..-1]
+    end
+end
+
 puts table.to_html
+
